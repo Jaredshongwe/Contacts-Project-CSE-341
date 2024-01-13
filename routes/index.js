7539b943a7f34f953c55fc8b28b47f5ae22d2180
Jaredshongwe <jaredshongwe@gmail.com>
2024-01-13 15:21:00 +0200

    Connected to Mongodb and added getAll and getSingle functions to project.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello World')});
+router.get('/', (req, res) => {res.send('Hello Friend')});
+
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
