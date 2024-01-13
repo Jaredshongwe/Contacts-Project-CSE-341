9cbc43f3d73fc45b227f6eca45f5727efc901801
Jaredshongwe <jaredshongwe@gmail.com>
2024-01-13 16:08:09 +0200

    renamed test file and added name to home page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello Friend')});
+router.get('/', (req, res) => {res.send(`Hello Friend, My name is Jared. Welcome to the Contacts Project home page`)});
 
 router.use('/contacts', require('./contacts'));
 
