ff6bf766041fddc199dec069cff6ec6b669cb033
Jaredshongwe <jaredshongwe@gmail.com>
2024-01-20 17:37:42 +0200

    Added POST, PUT and DELETE functions and updated .rest file.

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const mongodb = require('./data/database');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json())
 app.use('/', require('./routes'));
 
-
-mongodb.initDb((err)=>{
-    if(err){
+mongodb.initDb((err) => {
+    if (err) {
         console.log(err);
     }
-    else{
-        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+    else {
+        app.listen(port, () => { console.log(`Database is listening and node running on port ${port}`) });
     }
 })
