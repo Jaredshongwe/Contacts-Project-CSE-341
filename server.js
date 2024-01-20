a06898675f6e85e8d05779f14fae0e072675cd8c
Jaredshongwe <jaredshongwe@gmail.com>
2024-01-20 18:43:18 +0200

    Added swagger API docs

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-Width, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
@@ -15,4 +22,4 @@ mongodb.initDb((err) => {
     else {
         app.listen(port, () => { console.log(`Database is listening and node running on port ${port}`) });
     }
-})
+});
